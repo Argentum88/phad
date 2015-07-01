e4e5d3c7284038880a5bc6874800b4b0587c36bd
Argentum88 <andreyevpv@mail.ru>
2015-07-01 12:35:34 +0500

    fix install task

--- src/Tasks/InstallTask.php
+++ src/Tasks/InstallTask.php
@@ -14,6 +14,23 @@ class InstallTask extends Task
         mkdir($projectRoot . 'phad', 0755);
         copy(__DIR__ . '/../../templates/phad.php', $projectRoot . 'phad/phad.php');
 
-        copy(__DIR__ . '/../../templates/backend-assets', $projectRoot . 'public');
+        $this->recurseCopy(__DIR__ . '/../../templates/backend-assets', $projectRoot . 'public');
+        //copy(__DIR__ . '/../../templates/backend-assets', $projectRoot . 'public');
+    }
+
+    protected function recurseCopy($src,$dst) {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while(false !== ( $file = readdir($dir)) ) {
+            if (( $file != '.' ) && ( $file != '..' )) {
+                if ( is_dir($src . '/' . $file) ) {
+                    $this->recurseCopy($src . '/' . $file, $dst . '/' . $file);
+                }
+                else {
+                    copy($src . '/' . $file,$dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
     }
 }
